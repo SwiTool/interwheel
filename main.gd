3c423d649d1bff3c1c659867923ac397bb31789a
SwiTool <switooldev@gmail.com>
2025-08-10 17:12:39 +0200

    ♻️ clean code

--- main.gd
+++ main.gd
@@ -1,43 +1,36 @@
 extends Node
 
-const start_scene = preload('res://addons/kadokadeo_devkit/scenes/start.tscn')
 const game_scene = preload("res://game.tscn")
 var game: Node
-var contract: CanvasLayer
 
 @onready var start = $Start
-@onready var gameover = $GameOver
+@onready var end = $End
 
 func _ready() -> void:
 	set_process(false)
 	KadokadeoManager.initialize("7")
 	start.connect('start', _on_start_game)
-	gameover.connect('play_again', _on_game_over_play_again)
+	end.connect('play_again', _on_game_over_play_again)
 	new_game()
 
 func new_game():
 	start.visible = true
+	end.visible = false
 
 func _on_start_game() -> void:
 	game = game_scene.instantiate()
 	game.connect('ready', Callable(self, 'game_ready'))
-	game.connect('game_initialized', Callable(self, '_on_game_initialized'))
 	game.connect('game_finished', Callable(self, '_on_game_finished'))
 	$GameReceiver.add_child(game)
 
 func game_ready():
 	start.visible = false
 
-func _on_game_initialized() -> void:
-	pass
-
 func _on_game_finished() -> void:
-	print('game finished')
 	KadokadeoManager.end_game_session()
-	$GameOver.fade_in(2.0)
+	await end.trigger_end()
+	game.queue_free()
 
 func _on_game_over_play_again() -> void:
-	game.queue_free()
 	GameState.reset()
 	new_game()
-	$GameOver.fade_out(0.2)
